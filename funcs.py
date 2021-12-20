39f77099c43b86caf75465ee3caf355c7696a0eb
andrepnunes <a30nunes@gmail.com>
2021-12-20 22:27:33 +0100

    add avg feature : prototype + tests

--- funcs.py
+++ funcs.py
@@ -1,2 +1,5 @@
 def min (a, b):
 	return a if a < b else b
+
+def avg (int_lst):
+	return -1
--- test_funcs.py
+++ test_funcs.py
@@ -9,6 +9,12 @@ class TestFuncs(unittest.TestCase):
         self.assertEqual(funcs.min(3,3),3)
         self.assertEqual(funcs.min(-2,-1),-2)
         self.assertEqual(funcs.min(-1,-2),-2)
+
+    def test_avg(self):
+        self.assertEqual(funcs.avg([ -1,  0]),-0.5)
+        self.assertEqual(funcs.avg([  0,  0, 0]),0)
+        self.assertEqual(funcs.avg([  1,  2, 3]),2)
+        self.assertEqual(funcs.avg([ -1, -4, 2, 1]),-0.5)
         
 if __name__ == '__main__':
     unittest.main()
